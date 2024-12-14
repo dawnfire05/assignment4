969358727d6ca810993bf0a76369c3f519ff2feb
controlZ <o_dahyun@gm.gist.ac.kr>
2024-12-15 02:00:39 +0900

    ♻️ refactor: kibana에서 활용할 수 있도록 fielddata option 지정

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI(lifespan=lifespan)
 
 es = Elasticsearch("http://localhost:9200")
 
-DATABASE_PATH = "event_ticket_management_system.db"
+DATABASE_PATH = "/Users/dahyeon/develop/event_ticket_management_system/event_ticket_management_system.db"
 
 INDEX_NAME = "events"
 
@@ -26,23 +26,22 @@ def create_index():
             "mappings": {
                 "properties": {
                     "event_id": {"type": "integer"},
-                    "name": {"type": "text"},
-                    "description": {"type": "text"},
+                    "name": {"type": "text", "fielddata" : True},
+                    "description": {"type": "text", "fielddata" : True},
                     "category": {"type": "keyword"},
                     "ticket_open_time": {"type": "date"},
-                    "running_time": {"type": "integer"},
                     "venue": {
                         "properties": {
                             "venue_id": {"type": "integer"},
-                            "name": {"type": "text"},
-                            "location": {"type": "text"}
+                            "name": {"type": "text", "fielddata" : True},
+                            "location": {"type": "text", "fielddata" : True}
                         }
                     },
                     "sub_venue": {
                         "properties": {
                             "sub_venue_id": {"type": "integer"},
-                            "name": {"type": "text"},
-                            "capacity": {"type": "integer"}
+                            "name": {"type": "text", "fielddata" : True},
+                            "capacity": {"type": "integer"},
                         }
                     },
                     "schedules": {
@@ -55,8 +54,8 @@ def create_index():
                                 "type": "nested",
                                 "properties": {
                                     "artist_id": {"type": "integer"},
-                                    "name": {"type": "text"},
-                                    "company_name": {"type": "text"}
+                                    "name": {"type": "text", "fielddata" : True},
+                                    "company_name": {"type": "text", "fielddata" : True}
                                 }
                             }
                         }
@@ -79,7 +78,6 @@ def sync_data():
             e.description,
             e.category,
             e.ticket_open_time,
-            e.running_time,
             v.id as venue_id,
             v.name as venue_name,
             v.location,
@@ -97,8 +95,7 @@ def sync_data():
     for event in events:
         event_id = event[0]
 
-        # ISO 8601 형식으로 변환
-        ticket_open_time = datetime.strptime(event[4], "%Y-%m-%d %H:%M:%S").isoformat()
+        ticket_open_time = datetime.strptime(event[4], "%Y-%m-%dT%H:%M:%S.%fZ").isoformat()
 
         # 스케줄 데이터 읽기
         schedule_query = '''
@@ -118,8 +115,8 @@ def sync_data():
             schedule_id = schedule[0]
 
             # ISO 8601 형식으로 변환
-            start_time = datetime.strptime(schedule[1], "%Y-%m-%d %H:%M:%S").isoformat()
-            end_time = datetime.strptime(schedule[2], "%Y-%m-%d %H:%M:%S").isoformat()
+            start_time = datetime.strptime(schedule[1], "%Y-%m-%dT%H:%M:%S.%f").isoformat()
+            end_time = datetime.strptime(schedule[2], "%Y-%m-%dT%H:%M:%S.%f").isoformat()
 
             artist_query = '''
                 SELECT
@@ -153,16 +150,15 @@ def sync_data():
             "description": event[2],
             "category": event[3],
             "ticket_open_time": ticket_open_time,
-            "running_time": event[5],
             "venue": {
-                "venue_id": event[6],
-                "name": event[7],
-                "location": event[8]
+                "venue_id": event[5],
+                "name": event[6],
+                "location": event[7]
             },
             "sub_venue": {
-                "sub_venue_id": event[9],
-                "name": event[10],
-                "capacity": event[11]
+                "sub_venue_id": event[8],
+                "name": event[9],
+                "capacity": event[10]
             },
             "schedules": schedules_with_artists
         }
